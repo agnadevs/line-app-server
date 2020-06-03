dc12841d587f2e868b8a3adb06432af344debf03
karlnilsthure <nilsandersson.bass@gmail.com>
2020-06-03 09:15:44 +0200

    added some socket.io code

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,17 @@ var port = 4000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+io.on("connection", function (socket) {
+    socket.on("message", function (message) {
+        console.log(JSON.stringify(message));
+        var name = message.name, text = message.text;
+        io.emit("message", { text: text, name: name, userId: socket.id });
+    });
+    socket.on("disconnect", function (data) {
+        io.emit("admin-message", {
+            name: "Admin",
+            text: "A user has left the chat",
+        });
+    });
+});
 server.listen(port, function () { return console.log("listening on port " + port + ".."); });
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,25 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+interface ChatMessage {
+  name: string;
+  text: string;
+}
+
+io.on("connection", (socket: any) => {
+  socket.on("message", (message: ChatMessage) => {
+    console.log(JSON.stringify(message));
+    const { name, text } = message;
+
+    io.emit("message", { text, name, userId: socket.id });
+  });
+
+  socket.on("disconnect", (data: any) => {
+    io.emit("admin-message", {
+      name: "Admin",
+      text: "A user has left the chat",
+    });
+  });
+});
+
 server.listen(port, () => console.log(`listening on port ${port}..`));
